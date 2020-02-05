cc550ec38f1d3b46dd9c5c6a3cc09d3bd42fc380
Blendi Ibraj <blendi.ibraj96@gmail.com>
2020-02-05 22:05:17 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -11,6 +11,14 @@ function frameIt() {
   });
 }
 
+function pressIt() {
+  $('document').on('keydown', function(key){
+   if (key.which==71) {
+     alert('g was pressed');
+   }
+ })
+}
+
 
 $(document).ready(function(){
 
