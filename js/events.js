9b9035902c4098552f5d4d11fe8ca57e81a7d89e
Blendi Ibraj <blendi.ibraj96@gmail.com>
2020-02-05 22:04:09 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,16 @@
 //define functions here
+function getIt() {
+  $('p').on("click", function() {
+    alert("hey!")
+  });
+}
+
+function frameIt() {
+  $('img').on("load", function() {
+    $('img').addClass('tasty');
+  });
+}
+
 
 $(document).ready(function(){
 
